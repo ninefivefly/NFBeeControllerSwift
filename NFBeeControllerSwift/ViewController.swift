8ee600865b6675d0a5a33818517c0c47f7e418f4
ninefivefly <ninefivefly@foxmail.com>
2016-07-26 14:43:25 +0800

    Initial Commit

---
+++ NFBeeControllerSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NFBeeControllerSwift
+//
+//  Created by jiangpengcheng on 26/7/16.
+//  Copyright © 2016年 jiangpengcheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
